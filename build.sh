db86b8d8ad47109c7751e30b302bb003d776e105
Lucas Fernandez <contact@kasey.fr>
2017-11-30 23:23:43 +0100

    add animation on homepage and finsihed working titanic page

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 rm -rf output/* output-min/*
 
 staticjinja build --srcpath=templates --outpath=output
-cp -r js css output
-html-minifier --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
+cp -r js css img output
+#html-minifier --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
+#cp -r img output-min
