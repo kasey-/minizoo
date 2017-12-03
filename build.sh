a0c80c41d875c8d93f3964e0d549bdbdd00deab8
Lucas Fernandez <contact@kasey.fr>
2017-12-03 10:50:30 +0100

    add toggle navbar and cleanup build

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 rm -rf output/* output-min/*
 
 staticjinja build --srcpath=templates --outpath=output
-cp -r js css img output
+cp -r js css output
 #html-minifier --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
-#cp -r img output-min
+#cp -r output-min
---
+++ js/bulma-navbar.js
@@ -0,0 +1,16 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        console.log(target);
+        var $target = document.getElementById(target);
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
