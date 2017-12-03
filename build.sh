bcf610e51e2230f47906256b051c9ea46d110c50
Lucas Fernandez <contact@kasey.fr>
2017-12-03 14:45:21 +0100

    add to build minified output

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ rm -rf output/* output-min/*
 
 staticjinja build --srcpath=templates --outpath=output
 cp -r js css output
-#html-minifier --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
-#cp -r output-min
+html-minifier --file-ext html --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
+html-minifier --file-ext js --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
+html-minifier --file-ext css --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
+rm -r output-min/codes
+#cp -r js css output-min
