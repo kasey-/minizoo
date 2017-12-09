3a64657308081c78f59c37599826096b2d339d78
Lucas Fernandez <contact@kasey.fr>
2017-12-09 17:43:06 +0100

    fix rect issue + small cosmetic code refactoring

--- js/mnist.js
+++ js/mnist.js
@@ -1,36 +1,41 @@
 $(document).ready(function() {
-	var canvas  = document.querySelector( "#canvas" );
-	var context = canvas.getContext( "2d" );
+	var canvas  = document.querySelector('#canvas');
+	var context = canvas.getContext('2d');
 	canvas.width  = 280;
 	canvas.height = 280;
 
 	var Mouse = { x: 0, y: 0 };
 	var lastMouse = { x: 0, y: 0 };
-	context.fillStyle="white";
+	context.fillStyle = 'white';
 	context.fillRect(0,0,canvas.width,canvas.height);
-	context.color = "black";
+	context.color     = 'black';
 	context.lineWidth = 10;
-  context.lineJoin = context.lineCap = 'round';
+  context.lineJoin  = context.lineCap = 'round';
+	var rect = canvas.getBoundingClientRect();
 
-	canvas.addEventListener( "mousemove", function( e ) {
+	window.addEventListener('resize', function(event){
+    rect = canvas.getBoundingClientRect();
+  });
+
+	canvas.addEventListener('mousemove', function(e) {
 		lastMouse.x = Mouse.x;
 		lastMouse.y = Mouse.y;
-		Mouse.x = e.pageX - this.offsetLeft;
-		Mouse.y = e.pageY - this.offsetTop;
+		Mouse.x = e.pageX - rect.left;
+		Mouse.y = e.pageY - rect.top;
 	}, false );
 
-	canvas.addEventListener( "mousedown", function( e ) {
-		canvas.addEventListener( "mousemove", onPaint, false );
+	canvas.addEventListener('mousedown', function(e) {
+		canvas.addEventListener('mousemove', onPaint, false );
 	}, false );
 
-	canvas.addEventListener( "mouseup", function() {
-		canvas.removeEventListener( "mousemove", onPaint, false );
+	canvas.addEventListener('mouseup', function() {
+		canvas.removeEventListener('mousemove', onPaint, false );
 	}, false );
 
 	var onPaint = function() {
 		context.lineWidth   = context.lineWidth;
-		context.lineJoin    = "round";
-		context.lineCap     = "round";
+		context.lineJoin    = 'round';
+		context.lineCap     = 'round';
 		context.strokeStyle = context.color;
 
 		context.beginPath();
@@ -40,17 +45,17 @@ $(document).ready(function() {
 		context.stroke();
 	};
 
-  $('input[type="reset"]').on("click", function() {
+  $('input[type="reset"]').on('click', function() {
     context.clearRect( 0, 0, 280, 280 );
-    context.fillStyle="white";
+    context.fillStyle = 'white';
     context.fillRect(0,0,canvas.width,canvas.height);
 		$('#result').text('');
   });
 
-  $('input[type="submit"]').on("click", function(){
-    var canvasObj = document.getElementById("canvas");
+  $('input[type="submit"]').on('click', function(){
+    var canvasObj = document.getElementById('canvas');
     var img = canvasObj.toDataURL();
-    qwest.post("/mnist/number",{'img':img})
+    qwest.post('/mnist/number',{'img':img})
     .then(function(xhr, response) {
        $('#result').text('Predicted Output: '+response[1]);
     })
