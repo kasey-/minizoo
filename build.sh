97aa158d12d4cb73c9596fd9493438cbc480ccf5
Lucas Fernandez <lucas.fernandez@champ.aero>
2018-12-21 16:23:40 +0100

    prophet frontend 50% implemented

--- build.sh
+++ build.sh
@@ -8,10 +8,10 @@ staticjinja build --srcpath=templates --outpath=output
 cp -r img js css fonts output
 
 # Minify
-html-minifier --file-ext html --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
-html-minifier --file-ext css  --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
-cp -r img fonts output-min
-mkdir output-min/js
-for js in $(ls -1 js); do
-  terser js/$js -c -m > output-min/js/$js
-done
+#html-minifier --file-ext html --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
+#html-minifier --file-ext css  --input-dir output --output-dir output-min --remove-comments --collapse-whitespace --minify-js --minify-css
+#cp -r img fonts output-min
+#mkdir output-min/js
+#for js in $(ls -1 js); do
+#  terser js/$js -c -m > output-min/js/$js
+#done
