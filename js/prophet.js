375b500fda236a81d76256d8d8c4ec88836ff611
Lucas Fernandez <contact@kasey.fr>
2018-12-24 17:59:59 +0100

    First working fron

--- js/prophet.js
+++ js/prophet.js
@@ -1,18 +1,35 @@
-function ready(fn) {
-  if (document.readyState != 'loading'){
-    fn();
-  } else if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', fn);
-  } else {
-    document.attachEvent('onreadystatechange', function() {
-      if (document.readyState != 'loading')
-        fn();
-    });
-  }
-}
+const papa = require('papaparse');
+const axios = require('axios');
+const _ = require('lodash');
+const $ = require('cash-dom');
+const moment = require('moment');
+const dropzone = require('dropzone');
+const chartist = require('chartist');
+const lttb = require('downsample-lttb');
 
-ready(function(){
-  var myDropzone = new Dropzone("#uploadArea", {
+$(document).ready(function(){
+  const displayedSamples = 200;
+  const chartOpt = {
+    showPoint: false,
+    lineSmooth: true,
+    axisX: {
+      showGrid: false,
+      showLabel: true,
+      labelInterpolationFnc: function skipLabels(value, index) {
+        if(index % 400  === 0) {
+          if(value > 9999999999) value /= 1000;
+          return moment.unix(value).format('YYYY-DD-MM');
+        } else {
+          return null;
+        }
+      }
+    }
+  };
+
+  var chart1 = new chartist.Line('#chart1',{},chartOpt);
+  var chart2 = new chartist.Line('#chart2',{},chartOpt);
+
+  var myDropzone = new dropzone("#uploadArea", {
     url:'#',
     autoProcessQueue: false,
     acceptedFiles:'text/csv,application/vnd.ms-excel'
@@ -22,7 +39,7 @@ ready(function(){
     switch(file.type) {
       case 'text/csv':
       case 'application/vnd.ms-excel':
-        Papa.parse(file, {
+        papa.parse(file, {
           dynamicTyping: true,
           complete: function(results) {
             const {data} = results;
@@ -44,11 +61,34 @@ ready(function(){
             table = `${table}</tbody></table>`;
             $('#dataTable').html(table);
 
+/*
+            const data_conv_dates = data.map(function(value,index) {
+              return [,value[1]];
+            });
+
+            downsampled = lttb.processData(data_conv_dates, displayedSamples);
+            let label = downsampled.map(function(value,index) { return value[0]; });
+            let serie = downsampled.map(function(value,index) { return value[1]; });
+*/
+            let label = data.map(function(value,index) { return moment(value[0]).unix(); });
+            let serie = data.map(function(value,index) { return value[1]; });
+
+            chart1.update({
+              labels: label,
+              series: [serie]
+            });
+
             axios.post('http://127.0.0.1:5000/prophet/dataset', data)
               .then(function (response) {
-                console.log(response.data);
-                axios.get(`http://127.0.0.1:5000/prophet/${response.data.id}/predict/12`).then(function (response) {
-                  console.log(response.data);
+                axios.get(`http://127.0.0.1:5000/prophet/${response.data.id}/predict/365`).then(function (response) {
+                  const ds = Object.values(response.data.forecast.ds);
+                  const yhat = Object.values(response.data.forecast.yhat);
+                  const yhat_lower = Object.values(response.data.forecast.yhat_lower);
+                  const yhat_upper = Object.values(response.data.forecast.yhat_upper);
+                  chart2.update({
+                    labels: ds,
+                    series: [yhat,yhat_lower,yhat_upper]
+                  });
                 }).catch(function (error) {
                   console.log(error);
                 });
